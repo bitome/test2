e220e04b4192816e2d2e7e13a1e88c0c89140362
bitome <40987388+bitome@users.noreply.github.com>
2020-05-18 14:14:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ getData();
 async function getData(){
 
 	//const bag = [];
-    const response = await fetch("https://bitome.github.io/test2/animal.csv");
+    const response = await fetch("animal.csv");
     const data = await response.text();
     
     const species = data.split(',');
